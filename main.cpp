bc31d581be9a7e1f75b2bf294c02e01df012f8e0
Tony Phan <tphan022@hammer.cs.ucr.edu>
2016-02-07 01:27:09 -0800

    output works

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
+using namespace std;
+
+void display(char& argument) {
+	char* userinfo = getlogin();
+	char location[20];
+	gethostname(location,20);
+	cout << "<" << userinfo << "@" << location << ">" << "$";
+	
+}
+
+
+int main(void) {
+	char argument[1024];
+	display(*argument);
+
+return 0;
+}
