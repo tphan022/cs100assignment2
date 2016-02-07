a723b83325a2c422f507f8ca1a1eebe094d2bb7f
Tony Phan <tphan022@hammer.cs.ucr.edu>
2016-02-07 12:35:50 -0800

    now parsing into tokens work

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 using namespace std;
 
+
+// This display function displays the prompt and recieves the user input
 void display(char& argument) {
 	while(1) {
 		char* userinfo = getlogin();
@@ -19,10 +22,37 @@ void display(char& argument) {
 	}
 }
 
+void tokenizing(char& argument, vector<string>* v) {
+	char* ptr;
+	ptr = strtok(&argument, " \n");
+	string temp(ptr);
+	v->push_back(temp);
+	while(ptr != NULL) {
+		
+		ptr = strtok(NULL, " \n");
+		if(ptr != NULL) {
+			string temp2(ptr);
+			v->push_back(temp2);
+		}
+	}
+}
+
 
 int main(void) {
 	char argument[1024];
-	display(*argument);
+	vector<string> tokens;
+	while(1) {
+		display(*argument);
+		tokenizing(*argument,&tokens);
+		if(tokens.at(0) == "exit") {
+			break;
+		}
+		for(unsigned int i = 0; i < tokens.size(); i++) {
+			cout << tokens.at(i) << " ";
+		}
+		cout << endl;
+		tokens.clear();
+	}
 
 return 0;
 }
