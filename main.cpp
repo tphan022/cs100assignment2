87610c780cecdf6e7930718c344ebf932b43b920
Tony Phan <tphan022@hammer.cs.ucr.edu>
2016-02-07 17:58:41 -0800

    currently implementing connectors

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ void tokenizing(char& argument, vector<string>* v) {
 	}
 }
 
-void run(char** runcommand) {
+bool run(char** runcommand) {
 	pid_t PID;
 	int s;
 	if((PID = fork()) < 0) {
@@ -50,19 +50,44 @@ void run(char** runcommand) {
 	else if(PID == 0) {
 		if(execvp(*runcommand, runcommand) < 0) {
 			cout << "running error";
-			exit(1);
+			return false;
 		}
 	}
 	else {
 		while(wait(&s) != PID) {};
 	}
+	return true;
 }
 
-void 
+void connectors(vector<string>* v, char** command) {
+
+	int  successful = 0;
+	bool connected = false;
+	bool andconnect = false;
+	bool orconnect = false;
+	bool semicolon = false;
+	int i = 0;
+	int command_i = 0;
+	while(i < v->size()) {
+		if(strcmp(v->at(i), '#') == 0) {
+			break;
+		}
+		if(strcmp(v->at(i),"&&") == 0 && successful == 1 && !connected) {
+			andconnect = true;
+			connected = true;
+			i++;
+		}
+		else if(strcmp(v->at(i),"||") == 0 && 
+				successful == -1 && !connected); {
+			orconnect = true;
+	}
+		
+}
 
 int main(void) {
 	char argument[1024];
 	vector<string> tokens;
+	char* command[64];
 	while(1) {
 		display(*argument);
 		tokenizing(*argument,&tokens);
@@ -74,7 +99,6 @@ int main(void) {
 		}
 		cout << endl;
 		string tempo = tokens.at(0);
-		char* temp[64];
 		int q = 0;
 		temp[q] = (char*)tokens.at(0).c_str();
 		temp[q+1] = 0;
